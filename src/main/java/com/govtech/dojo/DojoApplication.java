f619a53af4b9474be4090c950dc52fd9b009451e
julia <juliatong2014thl@foxmail.com>
2020-11-19 16:21:52 +0800

    1st commit

---
+++ src/main/java/com/govtech/dojo/DojoApplication.java
@@ -0,0 +1,13 @@
+package com.govtech.dojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/govtech/dojo/DojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govtech.dojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DojoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
