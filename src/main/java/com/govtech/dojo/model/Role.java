c89cb434536958ef737ebf29787a464404ed2255
James Sia <cheekeong1985@gmail.com>
2020-11-27 00:07:29 +0800

    Add h2 database and jpa for repo

--- src/main/java/com/govtech/dojo/model/Role.java
+++ src/main/java/com/govtech/dojo/model/Role.java
@@ -1,22 +1,22 @@
 package com.govtech.dojo.model;
 
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name="role")
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="role")
 public class Role {
  
-// @Id
-// @GeneratedValue(strategy=GenerationType.AUTO)
-// @Column(name="role_id")
+ @Id
+ @GeneratedValue(strategy=GenerationType.AUTO)
+ @Column(name="role_id")
  private int id;
  
-// @Column(name="role")
+ @Column(name="role")
  private String role;
 
  public int getId() {
--- src/main/java/com/govtech/dojo/model/User.java
+++ src/main/java/com/govtech/dojo/model/User.java
@@ -2,42 +2,42 @@ package com.govtech.dojo.model;
 
 import java.util.Set;
 
-//import javax.persistence.CascadeType;
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.JoinTable;
-//import javax.persistence.ManyToMany;
-//import javax.persistence.Table;
-//
-//@Entity
-//@Table(name = "user")
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
 public class User {
  
-// @Id
-// @GeneratedValue(strategy = GenerationType.AUTO)
+ @Id
+ @GeneratedValue(strategy = GenerationType.AUTO)
  private int id;
  
-// @Column(name = "email")
+ @Column(name = "email")
  private String email;
  
-// @Column(name = "firstname")
+ @Column(name = "firstname")
  private String firstname; 
  
-// @Column(name = "lastname")
+ @Column(name = "lastname")
  private String lastname;
  
-// @Column(name = "password")
+ @Column(name = "password")
  private String password;
  
-// @Column(name = "active")
+ @Column(name = "active")
  private int active;
  
-// @ManyToMany(cascade=CascadeType.ALL)
-// @JoinTable(name="user_role", joinColumns=@JoinColumn(name="user_id"), inverseJoinColumns=@JoinColumn(name="role_id"))
+ @ManyToMany(cascade=CascadeType.ALL)
+ @JoinTable(name="user_role", joinColumns=@JoinColumn(name="user_id"), inverseJoinColumns=@JoinColumn(name="role_id"))
  private Set<Role> roles;
 
  public int getId() {
